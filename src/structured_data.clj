3edc33c72729af96c52496c914ae80e08ad2717e
code <cody@clearhealth.me>
2014-02-24 08:14:29 -0600

    through to conj assoc

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
+
 (defn do-a-thing [x]
-  :-)
+  (let [mm (+ x x)]
+    (Math/pow mm mm)))
 
 (defn spiff [v]
-  :-)
+  (cond
+   (> 3 (count v)) \?
+   :else (+ (get v 0) (get v 2))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,31 +24,39 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (= (- x2 x1) (- y2 y1))))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+   (* (- x2 x1) (- y2 y1))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x3 y3] point]
+      (and (<= x1 x3 x2) (<= y1 y3 y2)))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer]
+    (let [[[x3 y3] [x4 y4]] inner]
+      (and (contains-point? outer (point x3 y3)) (contains-point? outer (point x4 y4))))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (:authors book)) 1))
 
 (defn add-author [book new-author]
   :-)
